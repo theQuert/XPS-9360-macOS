502b0e7523e280a4bf32b968ea3ee7c9d3e5e1b7
Quert <a97041304@gmail.com>
2020-11-20 11:59:17 +0800

    Add trans.sh commands for migrating to OC

--- OC/trans.sh
+++ OC/trans.sh
@@ -1,4 +1,7 @@
 {
+    sudo diskutil mount /dev/disk0s1
+    rm -rf "/Volumes/EFI/EFI/CLOVER/drivers64UEFI/EmuVariableUefi-64.efi"
+    rm -rf "/Volumes/EFI/nvram.plist"
 	sudo rm -rf /Library/PreferencePanes/Clover.prefPane
 	rm -rf "/etc/rc.clover.lib"
 	rm -rf "/etc/rc.boot.d/10.save_and_rotate_boot_log.local"
@@ -9,7 +12,13 @@
 	rm -rf "/etc/rc.boot.d"
 	rm -rf "/etc/rc.shutdown.d"
 	launchctl unload '/Library/LaunchDaemons/com.slice.CloverDaemonNew.plist'
+    rm -rf "/Library/PreferencePanes/Clover.prefPane"
+    rm -rf "/Library/PreferencePanes/Clover.prefPane"
 	rm -rf '/Library/Application Support/Clover/CloverDaemonNew'
 	rm -rf '/Library/Application Support/Clover/CloverLogOut'
 	rm -rf '/Library/Application Support/Clover/CloverWrapper.sh'
+    sudo kextcache -i /
+    sudo -s
+    touch /Library/Extensions /System/Library/Extensions​
+    kextcache -i /​
 }
