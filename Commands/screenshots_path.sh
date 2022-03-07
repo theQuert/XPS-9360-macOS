f0acb426139da08828d8889039c67f9ab6e821fe
Quert <quert@MBP.local>
2022-03-07 10:34:06 +0800

    Update driver

--- Commands/screenshots_path.sh
+++ Commands/screenshots_path.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-defaults write com.apple.screencapture location ~/Pictures; killall SystemUIServer
+defaults write com.apple.screencapture location ~/Pictures/Screenshots; killall SystemUIServer
